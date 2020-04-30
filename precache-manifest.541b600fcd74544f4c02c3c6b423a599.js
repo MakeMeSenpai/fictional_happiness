e9d8e5e433210c8320b18295588ee06007ca59f1
Anthony <sean.a.protho1@gmail.com>
2020-04-30 14:07:32 -0700

    Updates

---
+++ precache-manifest.541b600fcd74544f4c02c3c6b423a599.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6236bca0e89c6e70cf79e9bcd2cc3451",
+    "url": "/fictional_happiness/index.html"
+  },
+  {
+    "revision": "092d913d3169806fcf62",
+    "url": "/fictional_happiness/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "a6720cdcb819f2b3b895",
+    "url": "/fictional_happiness/static/js/2.17724b57.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/fictional_happiness/static/js/2.17724b57.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "092d913d3169806fcf62",
+    "url": "/fictional_happiness/static/js/main.df12198d.chunk.js"
+  },
+  {
+    "revision": "e2361e063044f258c5a2",
+    "url": "/fictional_happiness/static/js/runtime-main.f959d808.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/fictional_happiness/static/media/logo.5d5d9eef.svg"
+  }
+]);
