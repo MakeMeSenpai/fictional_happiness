c914eb295f34998555612029ae5dda9e5dcfca34
Anthony <sean.a.protho1@gmail.com>
2020-05-02 00:10:31 -0700

    random images

--- src/Components/Game/Game.js
+++ src/Components/Game/Game.js
@@ -1,17 +1,40 @@
 // src/Components/Game/Game.js
 
 import React from 'react';
-import './Game.css'
+// import High from '../High/High';
+// import Score from '../Score/Score';
+// import Time from '../Time/Time';
+import './Game.css';
 
 function Game() {
+    // chooses a random image by id
+    let leftId = Math.floor(Math.random() *10)
+    let rightId = Math.floor(Math.random() * 10);
+
+    // determines which image will be a fake or real smile
+    let fakeOrReal = Math.random();
+    let leftChoice = '';
+    let rightChoice = '';
+    if (fakeOrReal >= 0.5) {
+        leftChoice = 'real';
+        rightChoice = 'fake';
+    } else {
+        leftChoice = 'fake';
+        rightChoice = 'real';
+    };
+
     return (
-        <div>
-            <code>From Game
-                needs left and right nameClass
-                needs counter function
-                randomly selected imgs
-                a Game over screen
-            </code>
+        <div className="content">
+             <img src={`${process.env.PUBLIC_URL}/imgs/${leftId}${leftChoice}.jpg`} width="300" alt={leftId} />
+             {/* placeholders for actual functions High, Score, & Time */}
+             <div className="center">
+                 <div>High-Score: 10</div>
+                 <div>Score: 3</div>
+                 {/* should be optional whether it's a timer
+                 or a counter */}
+                 <div>Time: 5 sec.</div> 
+             </div>
+             <img src={`${process.env.PUBLIC_URL}/imgs/${rightId}${rightChoice}.jpg`} width="300" alt={rightId} />
         </div>
     );
 }
--- src/Components/Title/Title.js
+++ src/Components/Title/Title.js
@@ -1,7 +1,5 @@
 // src/Components/Title/Title.js
 
-// import { NavLink } from 'react-router-dom';
-
 import React from 'react';
 import './Title.css';
 
@@ -12,19 +10,6 @@ function Title() {
           <title>Fictional Happiness</title>
           <h1 className="Title">Fictional Happiness</h1>
           <h3 className="Title-Subtitle">Can you tell a fake smile from a real one?</h3>
-  
-          {/* <div className="nav-link">
-            <NavLink
-                className="nav-link"
-                activeClassName="nav-link-active"
-                exact to="/">List</NavLink>
-
-            <NavLink
-                className="nav-link"
-                activeClassName="nav-link-active"
-                to="/about">About</NavLink>
-          </div> */}
-  
         </header>
       </div>
     )
